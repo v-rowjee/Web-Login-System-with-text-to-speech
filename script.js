6fcc84bb6f2e3da151f0b605a132afd4033f4bce
vedrowjee <rowjee.v@gmail.com>
2021-11-11 13:08:45 +0400

    Update structure

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//text-to-speech
+
 const url = "http://api.voicerss.org/";
 const apiKey = "9b687eec4a494bc78f6d372d5748514c";
 const buildUrl = (str) => `${url}?key=${apiKey}&hl=en-gb&src=${str}&v=Lily&r=2`;
